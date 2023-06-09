68c0089551a4cd54e3725c625b8d1c7158b97031
Calum Bird <calum@trelent.net>
2023-06-08 21:04:04 -0700

    Parallelize + gpt-4 :)

--- analysis/testing.py
+++ analysis/testing.py
@@ -15,11 +15,15 @@ def build_user_prompt(sentence: str, target: str):
     return prompt
 
 
-def determine_polarity(sentence: str, target: str):
+def determine_polarity(row):
+    target = row["mention"]
+    sentence = row["sentence"]
+
     user_prompt = build_user_prompt(sentence, target)
 
     response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
+        #model="gpt-3.5-turbo",
+        model="gpt-4",
         messages=[
             {"role": "system", "content": system_prompt},
             {"role": "user", "content": user_prompt},
@@ -29,34 +33,44 @@ def determine_polarity(sentence: str, target: str):
 
     output_text = response["choices"][0]["message"]["content"]
     splits = output_text.split("```")
+    
+    # Fucked up !?
+    if(len(splits) < 2):
+        return row
+    
     middle_part = splits[1]
     polarity = middle_part.strip()
-    return int(polarity)
+    # round to nearest integer, ie -0.5 to -1 and 0.5 to 1 and 0.1 to 0
+    pol_float = float(polarity)
+    if pol_float < -0.3:
+        polarity = -1
+    elif pol_float > 0.3:
+        polarity = 1
+    else:
+        polarity = 0
 
+    row["gpt4_polarity"] = polarity
+    return row
 
 dataset = datasets.load_dataset("fhamborg/news_sentiment_newsmtsc")
-test_data = dataset["test"].select(range(50))
-train_data = dataset["train"]
 
 # first_data_entry = data['test'][0]
 # print(first_data_entry)
 
+test_data = dataset["test"].select(range(30))
+test_data_with_gpt4 = test_data.map(determine_polarity, batched=False, num_proc=10)
+
 real_polarity_list = []
 gpt4_polarity_list = []
 
-for row in test_data:
+for row in test_data_with_gpt4:
     target = row["mention"]
     sentence = row["sentence"]
     real_polarity = row["polarity"]
-    gpt4_polarity = determine_polarity(sentence, target)
-
-    print("=" * 5)
-    print(f"Real polarity: {real_polarity}")
-    print(f"GPT-3 polarity: {gpt4_polarity}")
-    print("=" * 5)
-
-    real_polarity_list.append(real_polarity)
-    gpt4_polarity_list.append(gpt4_polarity)
+    if("gpt4_polarity" in row):
+        gpt4_polarity = row["gpt4_polarity"]
+        real_polarity_list.append(real_polarity)
+        gpt4_polarity_list.append(gpt4_polarity)
 
 missed = 0
 for idx in range(len(real_polarity_list)):
